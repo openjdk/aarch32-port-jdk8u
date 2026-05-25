cfce29a12b43596c3fee7fad6e2c66a3f1c0ac52
Sergey Nazarkin <snazarki@openjdk.org>
2026-05-25 12:35:02 +0000

    8385332: aarch32 started to crash after 8261235

--- hotspot/src/cpu/aarch32/vm/bytes_aarch32.hpp
+++ hotspot/src/cpu/aarch32/vm/bytes_aarch32.hpp
@@ -38,43 +38,133 @@ class Bytes: AllStatic {
   // AArch32 is little-endian, Java is big-endian; returns true
   static inline bool is_Java_byte_ordering_different(){ return true; }
 
+  static inline u2 get_Java_u2(address p) {
+    return (u2(p[0]) << 8) | u2(p[1]);
+  }
 
-  // Efficient reading and writing of unaligned unsigned data in platform-specific byte ordering.
-  // Since ARMv6 unaligned short and word accesses are handled by hardware.
-  // However, unaligned double-word access causes kernel trap and software processing,
-  // so we turn it to fast unalinged word access.
-  static inline u2   get_native_u2(address p)         { return *(u2*)p; }
-  static inline u4   get_native_u4(address p)         { return *(u4*)p; }
-  static inline u8   get_native_u8(address p)         {
-    if (!(uintptr_t(p) & 3)) {
-      return *(u8*)p;
+  static inline u4 get_Java_u4(address p) {
+    return u4(p[0]) << 24 |
+           u4(p[1]) << 16 |
+           u4(p[2]) <<  8 |
+           u4(p[3]);
+  }
+
+  static inline u8 get_Java_u8(address p) {
+    return u8(p[0]) << 56 |
+           u8(p[1]) << 48 |
+           u8(p[2]) << 40 |
+           u8(p[3]) << 32 |
+           u8(p[4]) << 24 |
+           u8(p[5]) << 16 |
+           u8(p[6]) <<  8 |
+           u8(p[7]);
+  }
+
+  static inline void put_Java_u2(address p, u2 x) {
+    p[0] = x >> 8;
+    p[1] = x;
+  }
+
+  static inline void put_Java_u4(address p, u4 x) {
+    ((u1*)p)[0] = x >> 24;
+    ((u1*)p)[1] = x >> 16;
+    ((u1*)p)[2] = x >>  8;
+    ((u1*)p)[3] = x;
+  }
+
+  static inline void put_Java_u8(address p, u8 x) {
+    ((u1*)p)[0] = x >> 56;
+    ((u1*)p)[1] = x >> 48;
+    ((u1*)p)[2] = x >> 40;
+    ((u1*)p)[3] = x >> 32;
+    ((u1*)p)[4] = x >> 24;
+    ((u1*)p)[5] = x >> 16;
+    ((u1*)p)[6] = x >>  8;
+    ((u1*)p)[7] = x;
+  }
+
+  static inline u2 get_native_u2(address p) {
+    return (intptr_t(p) & 1) == 0 ? *(u2*)p : u2(p[0]) | (u2(p[1]) << 8);
+  }
+
+  static inline u4 get_native_u4(address p) {
+    switch (intptr_t(p) & 3) {
+      case 0:  return *(u4*)p;
+      case 2:  return u4(((u2*)p)[0]) |
+                      u4(((u2*)p)[1]) << 16;
+      default: return u4(p[0])       |
+                      u4(p[1]) <<  8 |
+                      u4(p[2]) << 16 |
+                      u4(p[3]) << 24;
+    }
+  }
+
+  static inline u8 get_native_u8(address p) {
+    switch (intptr_t(p) & 7) {
+      case 0:  return *(u8*)p;
+      case 4:  return u8(((u4*)p)[0]) |
+                      u8(((u4*)p)[1]) << 32;
+      case 2:  return u8(((u2*)p)[0])       |
+                      u8(((u2*)p)[1]) << 16 |
+                      u8(((u2*)p)[2]) << 32 |
+                      u8(((u2*)p)[3]) << 48;
+      default: return u8(p[0])       |
+                      u8(p[1]) <<  8 |
+                      u8(p[2]) << 16 |
+                      u8(p[3]) << 24 |
+                      u8(p[4]) << 32 |
+                      u8(p[5]) << 40 |
+                      u8(p[6]) << 48 |
+                      u8(p[7]) << 56;
     }
     u4 *const a = (u4*) p;
     return (u8(a[1]) << 32) | a[0];
   }
 
-  static inline void put_native_u2(address p, u2 x)   { *(u2*)p = x; }
-  static inline void put_native_u4(address p, u4 x)   { *(u4*)p = x; }
-  static inline void put_native_u8(address p, u8 x)   { *(u8*)p = x; }
-
-
-  // Efficient reading and writing of unaligned unsigned data in Java
-  // byte ordering (i.e. big-endian ordering). Byte-order reversal is
-  // needed since AArch32 use little-endian format.
-  static inline u2   get_Java_u2(address p)           { return swap_u2(get_native_u2(p)); }
-  static inline u4   get_Java_u4(address p)           { return swap_u4(get_native_u4(p)); }
-  static inline u8   get_Java_u8(address p)           { return swap_u8(get_native_u8(p)); }
-
-  static inline void put_Java_u2(address p, u2 x)     { put_native_u2(p, swap_u2(x)); }
-  static inline void put_Java_u4(address p, u4 x)     { put_native_u4(p, swap_u4(x)); }
-  static inline void put_Java_u8(address p, u8 x)     {
-    const u8 nx = swap_u8(x);
-    if (!(uintptr_t(p) & 3)) {
-      *(u8*)p = nx;
+  static inline void put_native_u2(address p, u2 x) {
+    if ((intptr_t(p) & 1) == 0) {
+      *(u2*)p = x;
     } else {
-      u4 *const a = (u4*) p;
-      a[0] = nx;
-      a[1] = nx >> 32;
+      p[0] = x;
+      p[1] = x >> 8;
+    }
+  }
+
+  static inline void put_native_u4(address p, u4 x) {
+    switch (intptr_t(p) & 3) {
+      case 0:  *(u4*)p = x;
+               break;
+      case 2:  ((u2*)p)[0] = x;
+               ((u2*)p)[1] = x >> 16;
+               break;
+      default: ((u1*)p)[0] = x;
+               ((u1*)p)[1] = x >>  8;
+               ((u1*)p)[2] = x >> 16;
+               ((u1*)p)[3] = x >> 24;
+               break;
+    }
+  }
+
+  static inline void put_native_u8(address p, u8 x) {
+    switch (intptr_t(p) & 7) {
+      case 0:  *(u8*)p = x;
+               break;
+      case 4:  ((u4*)p)[0] = x;
+               ((u4*)p)[1] = x >> 32;
+               break;
+      case 2:  ((u2*)p)[0] = x;
+               ((u2*)p)[1] = x >> 16;
+               ((u2*)p)[2] = x >> 32;
+               ((u2*)p)[3] = x >> 48;
+               break;
+      default: ((u1*)p)[0] = x;
+               ((u1*)p)[1] = x >>  8;
+               ((u1*)p)[2] = x >> 16;
+               ((u1*)p)[3] = x >> 24;
+               ((u1*)p)[4] = x >> 32;
+               ((u1*)p)[5] = x >> 40;
+               ((u1*)p)[6] = x >> 48;
+               ((u1*)p)[7] = x >> 56;
     }
   }
 
